beef5744966094f7f647140b2df69b01d4dda656
John Gomez <johngomez1115@gmail.com>
2017-09-05 16:19:02 -0500

    Se borro el proyecto dentro del proyecto

---
+++ Assets/Scripts/MovimientoSwitche.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSwitche : MonoBehaviour {
+
+	[SerializeField]
+	private Animator anim;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			
+		}
+	}
+
+}
