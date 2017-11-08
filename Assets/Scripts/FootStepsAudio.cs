05911bd67e6eb62fabab8a84163aedd341634825
Johnny Chip <johngomez1115@gmail.com>
2017-11-08 09:02:05 -0500

    Camera animation and walk sounds

---
+++ Assets/Scripts/FootStepsAudio.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepsAudio : MonoBehaviour {
+
+	[SerializeField]
+	private AudioSource myAudioSource;
+
+	[SerializeField]
+	private Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start () {
+		myAudioSource.Pause();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.S)||Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.W))
+		{
+			if(Vector3.Magnitude(rb.velocity) > 0 && !myAudioSource.isPlaying)
+			{
+				myAudioSource.Play();
+			}
+		}
+
+		if(Vector3.Magnitude(rb.velocity)<=0.001f)
+		{
+			myAudioSource.Pause();
+		}else
+		{
+			if(!myAudioSource.isPlaying)
+			{
+				myAudioSource.Play();	
+			}
+		}
+
+		
+	}
+}
